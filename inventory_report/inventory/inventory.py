a858be3b25a155f18c666ca96d0d07d213d892e6
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-31 18:26:06 -0300

    refactor: Refatoração do método import_data, separação em mais métodos com reponsabilidades únicas

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,20 +8,38 @@ from inventory_report.reports.complete_report import CompleteReport
 
 class Inventory:
     @classmethod
-    def import_data(cls, file_path, report_type):
-        with open(file_path) as file:
-            if file_path.endswith(".csv"):
-                reader = csv.DictReader(file)
-                data = list(reader)
+    def read_csv(cls, file):
+        reader = csv.DictReader(file)
+        return list(reader)
 
-            elif file_path.endswith(".json"):
-                data = json.load(file)
+    @classmethod
+    def read_json(cls, file):
+        return json.load(file)
 
-            else:
-                reader = file.read()
-                data = xmltodict.parse(reader)["dataset"]["record"]
+    @classmethod
+    def read_xml(cls, file):
+        reader = file.read()
+        return xmltodict.parse(reader)["dataset"]["record"]
 
+    @classmethod
+    def generate_report(cls, data, report_type):
         if report_type == "simples":
             return SimpleReport.generate(data)
         elif report_type == "completo":
             return CompleteReport.generate(data)
+        else:
+            raise ValueError("Formato de arquivo inválido!")
+
+    @classmethod
+    def import_data(cls, file_path, report_type):
+        with open(file_path) as file:
+            if file_path.endswith(".csv"):
+                data = cls.read_csv(file)
+
+            elif file_path.endswith(".json"):
+                data = cls.read_json(file)
+
+            elif file_path.endswith(".xml"):
+                data = cls.read_xml(file)
+
+        return cls.generate_report(data, report_type)
