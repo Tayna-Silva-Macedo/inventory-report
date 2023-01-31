58002f1fcc3550c3ddc20ebe99ec9909e03a740c
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-31 17:07:36 -0300

    feat: Implementa a classe CompleteReport, gerando um relatório completo

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,6 +4,11 @@ from collections import Counter
 
 class SimpleReport:
     @classmethod
+    def companies_counter(cls, data):
+        companies = [product["nome_da_empresa"] for product in data]
+        return Counter(companies)
+
+    @classmethod
     def generate(cls, data):
         manufacturing_dates = [
             product["data_de_fabricacao"] for product in data
@@ -18,8 +23,7 @@ class SimpleReport:
         ]
         closest_expiration_date = sorted(expiration_dates)[0]
 
-        companies = [product["nome_da_empresa"] for product in data]
-        companie_most_common = Counter(companies).most_common()[0][0]
+        companie_most_common = cls.companies_counter(data).most_common()[0][0]
 
         return (
             f"Data de fabricação mais antiga: {earliest_manufacturing_date}\n"
